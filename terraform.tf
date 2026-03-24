fa984428dff5f888dbe2a70829820a997d3fd82c
oneamah <oneamah@gmail.com>
2026-03-24 16:07:12 -0400

    Remove cloud configuration from terraform.tf
    Removed cloud organization and workspace configuration.

--- terraform.tf
+++ terraform.tf
@@ -6,12 +6,6 @@ terraform {
         version = "~> 6.0"
     }
   }
-  cloud {
-    organization = "devops_terraform_1"
-    workspaces {
-      name = "devops_terraform1"
-    }
-  }
 }
 
 provider "aws" {
